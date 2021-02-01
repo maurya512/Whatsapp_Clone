cb8d918c57c497ba37a97aa6202f0fa3c2448cc2
Maurya Patel <patelmaurya0512@gmail.com>
2021-02-01 12:17:37 -0500

    The Chat component

---
+++ mern-whatsapp/src/Chat.js
@@ -0,0 +1,12 @@
+// the chat component
+import React from 'react'
+
+function Chat() {
+    return (
+        <div className="chat">
+            <h1>This is a chat component</h1>
+        </div>
+    )
+}
+
+export default Chat
