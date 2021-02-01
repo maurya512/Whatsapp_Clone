d9a1a869d530797cb1fc83873dcb48d44141bb24
Maurya Patel <patelmaurya0512@gmail.com>
2021-02-01 18:12:22 -0500

    added avatars

--- mern-whatsapp/src/Chat.js
+++ mern-whatsapp/src/Chat.js
@@ -1,10 +1,17 @@
 // the chat component
+import { Avatar } from '@material-ui/core'
 import React from 'react'
 
 function Chat() {
     return (
         <div className="chat">
-            <h1>This is a chat component</h1>
+            <div className="chat_header">
+                <Avatar />
+
+                <div className="chat_headerInfo">
+                    <h3>Room Name</h3>
+                </div>
+            </div>
         </div>
     )
 }
