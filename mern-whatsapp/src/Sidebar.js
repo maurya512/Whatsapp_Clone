0c6cb952ab8d0cbf01afb78e43f70601eafee97c
Maurya Patel <patelmaurya0512@gmail.com>
2021-02-01 18:12:56 -0500

    added header, search, and a chat section in the sidebar

--- mern-whatsapp/src/Sidebar.js
+++ mern-whatsapp/src/Sidebar.js
@@ -2,7 +2,11 @@ import React from 'react'
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { SearchOutlined } from '@material-ui/icons';
 import { IconButton, Avatar } from '@material-ui/core'
+
+// import components
+import Sidebarchat from './Sidebarchat';
 
 // import styling for sidebar
 import "./Sidebar.css";
@@ -14,7 +18,7 @@ function Sidebar() {
             {/* header of the sidebar */}
             <div className="sidebar_header">
                 {/* Avatar can be used to attach images */}
-                <Avatar src="https://avatars.githubusercontent.com/u/64752267?s=60&v=4"/>
+                <Avatar src="https://avatars.githubusercontent.com/u/64752267?s=60&v=4" />
                 {/* the right portion of the sidebar */}
                 <div className="sidebar_headerRight">
                     {/* Iconbutton makes the button clickable */}
@@ -32,10 +36,22 @@ function Sidebar() {
                         <MoreVertIcon />
                     </IconButton>
                 </div>
-                <div className="sidebar_search">
+            </div >
+            {/* the search bar */}
+            <div className="sidebar_search">
+                <div className="sidebar_searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="search or start new chat" type="text" />
                 </div>
             </div>
-        </div >
+
+            {/* chat section */}
+            <div className="sidebar_chats">
+                <Sidebarchat />
+                <Sidebarchat />
+                <Sidebarchat />
+            </div>
+        </div>
     )
 }
 
