32391a37961422e814e7fee7837b71fa79e8e4d3
Maurya Patel <patelmaurya0512@gmail.com>
2021-02-01 12:18:24 -0500

    The Sidebar component

---
+++ mern-whatsapp/src/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// import styling for sidebar
+import "./Sidebar.css";
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <h1>This is a sidebar</h1>
+        </div>
+    )
+}
+
+export default Sidebar
