f143ea717a5ba3335571b686aa4078b35f01b011
Maurya Patel <patelmaurya0512@gmail.com>
2021-02-01 20:03:21 -0500

    added footer element in chat

--- mern-whatsapp/src/Chat.js
+++ mern-whatsapp/src/Chat.js
@@ -1,6 +1,7 @@
 // the chat component
 import { Avatar, IconButton } from '@material-ui/core';
-import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
+import { AttachFile, InsertEmoticon, MoreVert, SearchOutlined } from '@material-ui/icons';
+import MicIcon from '@material-ui/icons/Mic';
 
 import './Chat.css';
 import React from 'react'
@@ -44,6 +45,97 @@ function Chat() {
                             .toUTCString()}
                     </span>
                 </p>
+
+                {/* this is a chat receiver */}
+                <p className="chat_message chat_receiver">
+                    {/* spanning the user's name */}
+                    <span className="chat_name">Maurya</span>
+                    This is a message
+
+                    {/* spanning the user's time stamp */}
+                    <span className="chat_timestamp">
+                        {new Date()
+                            .toUTCString()}
+                    </span>
+                </p>
+
+                <p className="chat_message">
+                    {/* spanning the user's name */}
+                    <span className="chat_name">Maurya</span>
+                    This is a message
+
+                    {/* spanning the user's time stamp */}
+                    <span className="chat_timestamp">
+                        {new Date()
+                            .toUTCString()}
+                    </span>
+                </p>
+
+                <p className="chat_message">
+                    {/* spanning the user's name */}
+                    <span className="chat_name">Maurya</span>
+                    This is a message
+
+                    {/* spanning the user's time stamp */}
+                    <span className="chat_timestamp">
+                        {new Date()
+                            .toUTCString()}
+                    </span>
+                </p>
+
+                <p className="chat_message">
+                    {/* spanning the user's name */}
+                    <span className="chat_name">Maurya</span>
+                    This is a message
+
+                    {/* spanning the user's time stamp */}
+                    <span className="chat_timestamp">
+                        {new Date()
+                            .toUTCString()}
+                    </span>
+                </p>
+
+                {/* this is a chat receiver */}
+                <p className="chat_message chat_receiver">
+                    {/* spanning the user's name */}
+                    <span className="chat_name">Maurya</span>
+                    This is a message
+
+                    {/* spanning the user's time stamp */}
+                    <span className="chat_timestamp">
+                        {new Date()
+                            .toUTCString()}
+                    </span>
+                </p>
+
+                {/* this is a chat receiver */}
+                <p className="chat_message chat_receiver">
+                    {/* spanning the user's name */}
+                    <span className="chat_name">Maurya</span>
+                    This is a message
+
+                    {/* spanning the user's time stamp */}
+                    <span className="chat_timestamp">
+                        {new Date()
+                            .toUTCString()}
+                    </span>
+                </p>
+            </div>
+            <div className="chat_footer">
+                <InsertEmoticon />
+                <form>
+                    <input
+                        // value={input}
+                        // onChange={(e) => setInput(e.target.value)}
+                        placeholder="Type a message here"
+                        type="text"
+                    />
+                    <button
+                    type="submit">
+                        Send
+                    </button>
+                </form>
+                <MicIcon />
             </div>
         </div>
     )
