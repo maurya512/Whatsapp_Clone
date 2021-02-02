1d464df288e222dd7289ff648e31c754bd56f066
Maurya Patel <patelmaurya0512@gmail.com>
2021-02-01 19:07:32 -0500

    added chat header, icons and chat message section

--- mern-whatsapp/src/Chat.js
+++ mern-whatsapp/src/Chat.js
@@ -1,5 +1,8 @@
 // the chat component
-import { Avatar } from '@material-ui/core'
+import { Avatar, IconButton } from '@material-ui/core';
+import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
+
+import './Chat.css';
 import React from 'react'
 
 function Chat() {
@@ -10,7 +13,37 @@ function Chat() {
 
                 <div className="chat_headerInfo">
                     <h3>Room Name</h3>
+                    <p>Last seen at</p>
                 </div>
+                <div className="chat_headerRight">
+                    <IconButton>
+                        {/* the search icon */}
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        {/* attach a file icon */}
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        {/* more options icon */}
+                        <MoreVert />
+                    </IconButton>
+                </div>
+            </div>
+
+            {/* chat body */}
+            <div className="chat_body">
+                <p className="chat_message">
+                    {/* spanning the user's name */}
+                    <span className="chat_name">Maurya</span>
+                    This is a message
+
+                    {/* spanning the user's time stamp */}
+                    <span className="chat_timestamp">
+                        {new Date()
+                            .toUTCString()}
+                    </span>
+                </p>
             </div>
         </div>
     )
